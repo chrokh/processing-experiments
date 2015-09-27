0e83b9df086ae2f2f5712db048fb227cfd8c9088
chrokh <christopher.okhravi@gmail.com>
2015-09-27 18:52:03 +0200

    Refactor for clarity

--- sketch_03_particles/Explosion.java
+++ sketch_03_particles/Explosion.java
@@ -7,35 +7,39 @@ class Explosion extends ParticleAnimation {
   List<ISpawnerGroup> groups = new ArrayList<ISpawnerGroup>();
 
   public Explosion() {
+    _addGroup(_trailingSmokeSpawners());
+    _addGroup(_pieceSpawners());
+    _addGroup(_smokeSpawners());
+  }
 
-    {
-      int trailingSmokeCount = 1;
-      List<ISpawner> spawners = new ArrayList<ISpawner>();
-      for(int i=0; i<9; i++)
-        spawners.add(new TrailingSmoke());
-      ISpawnerGroup group = new SpawnerGroup(spawners);
-      this.groups.add(group);
-    }
+  public List<ISpawner> _trailingSmokeSpawners() {
+    List<ISpawner> spawners = new ArrayList<ISpawner>();
+    int trailingSmokeCount = 1;
+    for(int i=0; i<9; i++)
+      spawners.add(new TrailingSmoke());
+    return spawners;
+  }
 
+  public List<ISpawner> _pieceSpawners() {
+    List<ISpawner> spawners = new ArrayList<ISpawner>();
+    int pieceCount = (int)App.get().random(3, 12);
+    for(int i=0; i<pieceCount; i++)
+      spawners.add(new PieceSpawner(3));
+    return spawners;
+  }
+
+  public List<ISpawner> _smokeSpawners() {
+    List<ISpawner> spawners = new ArrayList<ISpawner>();
     int numSpawners = 40;
     int numSpawned  = 4;
-    for(int i=0; i<numSpawners; i++) {
-
-      List<ISpawner> spawners = new ArrayList<ISpawner>();
+    for(int i=0; i<numSpawners; i++)
       spawners.add(new SmokeSpawner(numSpawned));
+    return spawners;
+  }
 
-      ISpawnerGroup group = new SpawnerGroup(spawners);
-
-      this.groups.add(group);
-    }
-
-    int pieceCount = (int)App.get().random(3, 12);
-    for(int i=0; i<pieceCount; i++) {
-      List<ISpawner> spawners = new ArrayList<ISpawner>();
-      spawners.add(new PieceSpawner(3));
-      ISpawnerGroup group = new SpawnerGroup(spawners);
-      this.groups.add(group);
-    }
+  public void _addGroup(List<ISpawner> spawners) {
+    ISpawnerGroup group = new SpawnerGroup(spawners);
+    this.groups.add(group);
   }
 
   public List<ISpawnerGroup> getGroups() {
